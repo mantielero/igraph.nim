3c547b8e31c01abe1cf8b4b7901704ac638c5834
mantielero <josemaria.alkala@gmail.com>
2024-06-02 23:32:50 +0200

    to uppercase

--- src/lib/defs.nim
+++ src/lib/defs.nim
@@ -1,26 +1,32 @@
 import ../wrapper/igraph_wrapper
 
 
-let
-  success* = Igraphsuccess
+const
+  SUCCESS* = Igraphsuccess
 
-  directed*   = Igraphdirected
-  undirected* = Igraphundirected
 
+const
   # enumigraphloopst
-  noLoops*   = Igraphnoloops
-  loops*     = Igraphloops
-  loopsOnce* = Igraphloopsonce
+  NOLOOPS*   = Igraphnoloops
+  LOOPS*     = Igraphloops
+  LOOPSONCE* = Igraphloopsonce
 
+const
+  DIRECTED*   = Igraphdirected
+  UNDIRECTED* = Igraphundirected
 
-
-converter toOther*(data:seq[int]):igraph_vector_int_t =
+converter toVectorViewInt*(data:seq[int]):igraph_vector_int_t =
   var tmp:igraph_vector_int_t
-  var ret = igraph_vector_int_view(tmp.addr, 
-                         cast[ptr igraphintegert](data[0].addr), 
-                         data.len.igraphintegert)
-  # if ret != Igraphsuccess:
-  #   raise newException(ValueError, "failed" ) # igrapherrort  
+  #igraph_vector_int_init(tmp.addr, data.len)
+  if data.len > 0:
+    var ret = igraph_vector_int_view(tmp.addr, 
+                          cast[ptr igraphintegert](data[0].addr), 
+                          data.len.igraphintegert)
+    # echo "STOREBEGIN: ", ret.storbegin[]
+    # echo "STOREEND: ", ret.storend[]
+    # echo "ENDFIELD: ", ret.endfield[]    
+    #if ret != SUCCESS:
+    #  raise newException(ValueError, "failed" ) # igrapherrort  
   return tmp
 
 converter toIgraphInt*(val:int):igraphintegert =
@@ -34,3 +40,18 @@ converter toIgraphBool*(val:enumigraphidirectedt):igraphboolt =
 
 # converter toPointerInt*(val:int):ptr igraphintegert =
 #   (val.igraphintegert).addr
+
+
+# Sequence to structigraphvectorintt
+# converter toIgraphVectorInt*(vec:seq[int]):igraphvectorintt =
+#   result = new igraphvectorintt
+#   if vec.len > 0:
+#     result.storebegin = cast[ptr igraphintegert](vec[0].addr)
+
+
+
+#[
+    storbegin*: ptr igraphintegert ## Generated based on /usr/include/igraph/igraph_vector_type.h:29:16
+    storend*: ptr igraphintegert
+    endfield*: ptr igraphintegert
+]#
