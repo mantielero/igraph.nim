5272ce2a46be11890888a893415ea79e2c459cd6
mantielero <josemaria.alkala@gmail.com>
2024-06-10 23:25:47 +0200

    function rename plus cleaning

--- src/lib/generators.nim
+++ src/lib/generators.nim
@@ -1,5 +1,6 @@
 import ../wrapper/igraph_wrapper
 import defs, graph, basic_interface
+import std/strformat
 # Chapter 9. Graph generators
 # https://igraph.org/c/doc/igraph-Generators.html
 
@@ -21,53 +22,6 @@ proc newSmall*(n:int; directed:enumigraphidirectedt; values: varargs[int]):Graph
     result.addEdge(values[i*2],values[i*2+1])
 
 
-#[
-    igraph_vector_int_t edges;
-    va_list ap;
-
-    IGRAPH_VECTOR_INT_INIT_FINALLY(&edges, 0);
-
-    va_start(ap, first);
-    int num = first;
-    while (num != -1) {
-        igraph_vector_int_push_back(&edges, num);
-        num = va_arg(ap, int);
-    }
-    va_end(ap);
-
-    IGRAPH_CHECK(igraph_create(graph, &edges, n, directed));
-
-    igraph_vector_int_destroy(&edges);
-    IGRAPH_FINALLY_CLEAN(1);
-    return IGRAPH_SUCCESS;
-]#
-
-# https://github.com/igraph/igraph/blob/c0f15367e471ff6f93d73100de9eb4b37ecdbd05/src/constructors/basic_constructors.c#L136-L156
-
-#proc newSmall*(n:int; directed:enumigraphidirectedt; values: varargs[int]):Graph =
-#[
-igraph_error_t igraph_small(igraph_t *graph, igraph_integer_t n, igraph_bool_t directed,
-                            int first, ...) {
-    igraph_vector_int_t edges;
-    va_list ap;
-
-    IGRAPH_VECTOR_INT_INIT_FINALLY(&edges, 0);
-
-    va_start(ap, first);
-    int num = first;
-    while (num != -1) {
-        igraph_vector_int_push_back(&edges, num);
-        num = va_arg(ap, int);
-    }
-    va_end(ap);
-
-    IGRAPH_CHECK(igraph_create(graph, &edges, n, directed));
-
-    igraph_vector_int_destroy(&edges);
-    IGRAPH_FINALLY_CLEAN(1);
-    return IGRAPH_SUCCESS;
-}
-]#
 # 1.3. igraph_adjacency — Creates a graph from an adjacency matrix.
 # 1.4. igraph_weighted_adjacency — Creates a graph from a weighted adjacency matrix.
 # 1.5. igraph_sparse_adjacency — Creates a graph from a sparse adjacency matrix.
@@ -88,13 +42,21 @@ proc newStar*(n:int; mode:igraph_star_mode_t; center:int):Graph =
 # 1.11. igraph_triangular_lattice — A triangular lattice with the given shape.
 # 1.12. igraph_hexagonal_lattice — A hexagonal lattice with the given shape.
 # 1.13. igraph_ring — Creates a cycle graph or a path graph.
+
+proc newRing*(n:int; directed:bool; mutual:bool; circular:bool):Graph =
+  # https://igraph.org/c/doc/igraph-Generators.html#igraph_ring
+  result = new Graph
+  var ret = igraph_ring(result.handle.addr, n, directed, mutual, circular)
+  if ret != Igraphsuccess:
+    raise newException(ValueError, &"error while calling the function: {ret}")
+
 # 1.14. igraph_kary_tree — Creates a k-ary tree in which almost all vertices have k children.
 # 1.15. igraph_symmetric_tree — Creates a symmetric tree with the specified number of branches at each level.
 # 1.16. igraph_regular_tree — Creates a regular tree.
 
 # 1.17. igraph_full — Creates a full graph (complete graph).
 # https://igraph.org/c/doc/igraph-Generators.html#igraph_full
-proc full*(n:int;directed:bool; loops:enumigraphloopst):Graph =
+proc newFull*(n:int; directed:enumigraphidirectedt ; loops:enumigraphloopst):Graph =
   result = new Graph
   var ret = igraph_full(result.handle.addr, n, directed, loops.igraphboolt)
   if ret != SUCCESS:
